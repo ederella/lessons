cf8461c3225d0d631d55fef780c6fb0ecef9a986
Veronica Alexeevskaya <ederella@yandex.ru>
2024-03-09 13:30:59 +0300

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,15 +4,15 @@ public class BinarySearch {
 
 	int Left;
 	int Right;
-	private int[]arr;
+	private int[] arr;
 	private int found;
-	
+
 	public BinarySearch(int[] arr) {
 		this.Left = 0;
 		this.Right = arr.length - 1;
 		this.arr = arr;
 	}
-	
+
 	public void Step(int N) {
 		int central = (Right + Left) / 2;
 
@@ -38,9 +38,42 @@ public class BinarySearch {
 			return;
 		}
 	}
-	
+
 	public int GetResult() {
 		return found;
 	}
-	
+
+	public boolean GallopingSearch(int[] arr, int N) {
+		if (arr == null || arr.length == 0)
+			return false;
+
+		int index = 0;
+		int i = 1;
+		while (true) {
+
+			if (arr[index] == N)
+				return true;
+
+			if (index == arr.length - 1 && arr[index] < N)
+				return false;
+
+			if (arr[index] > N) {
+				break;
+			}
+
+			index = (int) (Math.pow(2, ++i) - 2);
+
+			if (index >= arr.length)
+				index = arr.length - 1;
+		}
+		this.found = 0;
+		this.Left = (int) (Math.pow(2, i - 1) - 2) + 1;
+		this.Right = index;
+		
+		while (this.found == 0) {
+			Step(N);
+		}
+
+		return this.found == 1;
+	}
 }
