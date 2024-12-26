df7440e79ed4a4cbf30a494956afc3430b03581a
Veronica Alexeevskaya <ederella@yandex.ru>
2024-12-26 18:42:50 +0300

    Create issue12.fs

---
+++ F-sharp/issue12.fs
@@ -0,0 +1,20 @@
+// 34.1
+let rec upto x = 
+    let rec uptofun = function
+        |(0, ls) -> ls
+        |(x, ls) -> uptofun(x-1, x::ls)
+    uptofun(x, [])
+
+// 34.2
+let rec dnto x = 
+    let rec dntofun = function
+        |(1, ls) -> 1::ls
+        |(x, ls) -> x::dntofun(x-1, ls)
+    dntofun(x, [])
+
+// 34.3
+let rec evenn x = 
+    let rec evennfun = function
+        |(0, ls) -> ls
+        |(x, ls) -> evennfun(x-1, (2*x)::ls)
+    evennfun(x, [])
