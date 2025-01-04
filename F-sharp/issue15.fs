1cac6932bb677f0fb5d0f966bd00cb0a4165e6b4
Veronica Alexeevskaya <ederella@yandex.ru>
2025-01-04 20:19:36 +0300

    Update issue15.fs

--- F-sharp/issue15.fs
+++ F-sharp/issue15.fs
@@ -1,17 +1,11 @@
 // 41.4.1
 let list_filter f xs = 
-  let ff = fun x y -> 
-    if f(x)
-      then x::y 
-    else y
+  let ff = fun x y -> if f(x) then x::y else y
   List.foldBack ff xs []
 
 // 41.4.2
 let sum p xs = 
-  let ff = fun x y ->
-    if p(x)
-      then x + y 
-    else x
+  let ff = fun x y -> if p(x) then x + y else y
   List.foldBack ff xs 0
 
 // 41.4.3
