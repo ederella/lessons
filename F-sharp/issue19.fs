8478977db46c2ed37314f5d0d16092783b97f8bb
Veronica Alexeevskaya <ederella@yandex.ru>
2025-01-15 23:20:35 +0300

    Update issue19.fs

--- F-sharp/issue19.fs
+++ F-sharp/issue19.fs
@@ -2,13 +2,13 @@
 let rec fibo1 n n1 n2 = 
     if n = 0 then n2
     else if n = 1 then n1
-    else fibo1 (n-1) (n1+n2) n1
+    else fibo1 (n-1) (n1 + n2) n1
 
 // 48.4.2
 let rec fibo2 n c = 
     if n <= 1 then c n
-    else 
-        fibo2 (n - 1) (fun n1 -> fibo2 (n-2) (fun n2 -> c (n1 + n2)))
+    else fibo2 (n-1) (fun n1 -> fibo2 (n-2) (fun n2 -> c (n1 + n2)))
+    
 // 48.4.3
 let rec bigList n k = 
     if n=0 then k []
