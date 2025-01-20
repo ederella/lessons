d1c4611aa984890731d1bb3aa91dee14b3da4e16
Veronica Alexeevskaya <ederella@yandex.ru>
2025-01-20 11:34:05 +0300

    Update issue21.fs

--- F-sharp/issue21.fs
+++ F-sharp/issue21.fs
@@ -1,16 +1,11 @@
 // 50.2.1
-let fac_seq n = seq{
-                  let rec fact = function
+let fac_seq n = seq{let rec fact = function
                       |0|1 -> 1
                       |x -> x*fact(x-1)
-                  for i in 0..n do
-                      yield fact i
-                  }
- 
- // 50.2.2
-let seq_seq n = seq{
-                   yield 0
-                   for i in 1..n do
-                     yield i
-                     yield -i
-                    }
+                    for i in 0..n do
+                      yield fact i}
+// 50.2.2
+let seq_seq n = seq{ yield 0
+                     for i in 1..n do
+                       yield i
+                       yield -i}
