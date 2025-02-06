fb714d76734eeba61ebfe2c315352fcc5e123c1a
Veronica Alexeevskaya <ederella@yandex.ru>
2025-02-06 19:44:54 +0300

    task2

---
+++ SoftwareDesign/account/BankAccount.java
@@ -0,0 +1,23 @@
+package account;
+
+public class BankAccount {
+
+	private double balance;
+	
+	
+	public BankAccount(double balance) {
+		this.balance = balance;
+	}
+	
+	public void deposit(double amount) {
+		balance +=amount;
+	}
+	
+	public void withdraw(double amount) {
+		balance -=amount;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+}
---
+++ SoftwareDesign/account/BankAccountTest.java
@@ -0,0 +1,36 @@
+package account;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BankAccountTest {
+
+	BankAccount bankAccount;
+
+	@BeforeEach
+	public void setUp() {
+		bankAccount = new BankAccount(1000.00);
+	}
+
+	@Test
+    public void testDeposit() {
+        assertDoesNotThrow(() -> bankAccount.deposit(1_000_000_000.00));
+        System.out.println("after deposit 1_000_000_000.00 : " + bankAccount.getBalance());
+        
+        assertDoesNotThrow(() -> bankAccount.deposit(-10_000_000_000.00));
+        System.out.println("after deposit -10_000_000_000.00 : " +bankAccount.getBalance());
+     
+    }
+
+	@Test
+    public void testWithdraw() {
+        assertDoesNotThrow(() -> bankAccount.withdraw(1_000_000_000));
+        System.out.println("after withdraw 1_000_000_000.00 : " + bankAccount.getBalance());
+        
+        assertDoesNotThrow(() -> bankAccount.withdraw(-1_000_000_000));
+        System.out.println("after withdraw -1_000_000_000.00 : " +bankAccount.getBalance());
+    }
+	
+}
