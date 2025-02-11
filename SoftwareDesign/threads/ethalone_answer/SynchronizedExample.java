5babd9a6deace61aa51305157fb9ac2695284e5a
Veronica Alexeevskaya <ederella@yandex.ru>
2025-02-11 21:13:47 +0300

    answers

---
+++ SoftwareDesign/threads/ethalone_answer/SynchronizedExample.java
@@ -0,0 +1,34 @@
+package threads;
+
+public class SynchronizedExample {
+
+    private static int counter = 0;
+
+    public static synchronized void increment() {
+        counter++;
+    }
+
+    public static void main(String[] args) {
+        int numberOfThreads = 10;
+        Thread[] threads = new Thread[numberOfThreads];
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new Thread(() -> {
+                for (int j = 0; j < 100000; j++) {
+                    increment();
+                }
+            });
+            threads[i].start();
+        }
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("Final counter value: " + counter);
+    }
+}
