d4d95cb82542c83f7d9f87317b21de5f2825110d
Veronica Alexeevskaya <ederella@yandex.ru>
2025-02-25 21:07:20 +0300

    Update HoareTriples.java

--- SoftwareDesign/hoare/HoareTriples.java
+++ SoftwareDesign/hoare/HoareTriples.java
@@ -1,25 +1,34 @@
 package hoare;
 
 public class HoareTriples {
-
-	/**
-	 P{a = n, b = m} maxOfAbs(a,b) Q{res = max(abs(n), abs(m)}
-	 P{a = n, b = m} max(a, b) Q{res = если n > m то n иначе m}
-	 P{a = n} abs(a) Q{res = если n > 0 то n иначе -n}
-	 */
 	
 	public static void main(String[] args) {
 		System.out.println(maxOfAbs(5, -100));
 	}
-	
+
+	/**
+ 	{P: true} 
+  	int maxAbs(int a, int b) 
+   	{Q: result = max(|a|, |b|)}
+ 	*/
 	public static int maxOfAbs(int a, int b) {
 		return max(abs(a), abs(b));
 	}
+
+	/**
+	{P: true} 
+ 	int max(int a, int b) 
+	{Q: ((result = a) or (result = b)) and (result >= a and result >= b)}
+	 */
 	
 	private static int max(int a, int b) {
 		return a > b ? a : b;
 	}
-
+	/**
+ 	{P: true} 
+  	int abs(int x) 
+   	{Q: result = |x|}
+	*/
 	private static int abs(int x) {
 		return x > 0 ? x : -x;
 	}
