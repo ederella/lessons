04e6688ba8a211981844044c19d61bdfb58b169a
Veronica Alexeevskaya <ederella@yandex.ru>
2025-10-06 22:03:32 +0300

    Update Game29052925.cs

--- FunctionalDesign/Game29052925.cs
+++ FunctionalDesign/Game29052925.cs
@@ -1,21 +1,24 @@
 public static Func<BoardState, BoardState> BuildGamePipeline(bool debugMode)
-        {
-            return bs => bs
+{
+        return bs => bs
                     .Draw(debugMode)
                     .Pipe(FillEmptySpaces)
                     .Draw(debugMode)
                     .Pipe(ProcessCascade);
-        }
+}
 
 public static BoardState ProcessCascade(BoardState currentState)
 {
-    return currentState
-            .Pipe(state => FindMatches(currentState))
-            .Pipe(matches => matches.Count == 0
+        bool debugMode = true;
+        return FindMatches(currentState.Board).Count == 0
                 ? currentState
                 : currentState
-                    .Pipe(s => RemoveMatches(s, matches))
-                    .Pipe(ApplyGravity)
+                    .Pipe(bs =>
+                    {
+                        return RemoveMatches(bs, FindMatches(bs.Board));
+                    })
+                    .Draw(debugMode)
                     .Pipe(FillEmptySpaces)
-                    .Pipe(ProcessCascade));
+                    .Draw(debugMode)
+                    .Pipe(ProcessCascade);
 }
