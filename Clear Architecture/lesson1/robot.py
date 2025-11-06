11102f3bd7d2b6a169bbf2a75c2cfb8d968fc776
Veronica Alexeevskaya <ederella@yandex.ru>
2025-11-06 22:11:04 +0300

    Create robot.py

---
+++ Clear Architecture/lesson1/robot.py
@@ -0,0 +1,45 @@
+import math
+
+class Robot:
+    def __init__(self) :
+        self.__pos = (0,0)
+        self.__angle = 0
+        self.__state = 'water'
+        self.__is_working = False
+
+    def move(self, path: int) :
+        x = path * math.sin(self.__angle)
+        y = path * math.cos(self.__angle)
+        self.__pos = (x, y)
+        print('POS ' + str(self.__pos))
+
+    def turn(self, angle) :
+        self.__angle = angle
+        print('ANGLE ' + str(angle))
+
+    def set(self, state):
+        self.__state = state
+        print('STATE ' + str(state))
+
+    def start(self) :
+        self.__is_working = True
+        print('START WITH ' + self.__state)
+
+    def stop(self) :
+        self.__is_working = False
+        print('STOP')
+
+robot = Robot()
+command = ('','')
+while command[0] != 'quit':
+    command = input('Введите команду: ').split(' ')
+    if command[0] == 'move':
+        robot.move(int(command[1]))
+    if command[0] == 'turn':
+        robot.turn(int(command[1]))
+    if command[0] == 'set':
+        robot.set(str(command[1]))
+    if command[0] == 'start':
+        robot.start()
+    if command[0] == 'stop':
+        robot.stop()
